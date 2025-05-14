45be855cab0ffa070a5e43ac7d724c66463b756d
Ousmane Sarr Diop <ousmane.diop.1@etu.u-bordeaux.fr>
2025-05-14 20:10:08 +0200

    Initial commit

---
+++ src/test/java/com/example/trust/TrustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
