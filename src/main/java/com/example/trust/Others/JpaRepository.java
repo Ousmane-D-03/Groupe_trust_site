1a92dc108dd72f681f50db33348687bcf78c9656
Ousmane Sarr Diop <ousmane.diop.1@etu.u-bordeaux.fr>
2025-05-15 01:57:20 +0200

    encapsulation des services

---
+++ src/main/java/com/example/trust/Others/JpaRepository.java
@@ -0,0 +1,5 @@
+package com.example.trust.Others;
+
+public class JpaRepository<T1, T2> {
+
+}
--- src/main/java/com/example/trust/TrustApplication.java
+++ src/main/java/com/example/trust/TrustApplication.java
@@ -1,4 +1,4 @@
-package com.example.trust.Others;
+package com.example.trust;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
